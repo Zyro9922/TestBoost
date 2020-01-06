d96889148bc7a350b4a95c00ae0ab4e72b864c47
Zyro9922 <alihasan9922@gmail.com>
2020-01-07 03:32:25 +0530

    New

--- include/boost/astronomy/coordinate/coordinate.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BOOST_ASTRONOMY_COORDINATE_HPP
-#define BOOST_ASTRONOMY_COORDINATE_HPP
-
-#include <boost/astronomy/coordinate/representation.hpp>
-#include <boost/astronomy/coordinate/differential.hpp>
-#include <boost/astronomy/coordinate/frame.hpp>
-
-#endif // !BOOST_ASTRONOMY_COORDINATE_HPP
-
--- include/boost/astronomy/coordinate/representation.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BOOST_ASTRONOMY_COORDINATE_REPRESENTATION_HPP
-#define BOOST_ASTRONOMY_COORDINATE_REPRESENTATION_HPP
-
-#include <boost/astronomy/coordinate/cartesian_representation.hpp>
-#include <boost/astronomy/coordinate/spherical_equatorial_representation.hpp>
-#include <boost/astronomy/coordinate/spherical_representation.hpp>
-
-#endif // !BOOST_ASTRONOMY_COORDINATE_REPRESENTATION_HPP
-
