546e1a671972f09f2415dc6f912838a1ce4b2e21
Zyro9922 <alihasan9922@gmail.com>
2020-01-16 16:37:52 +0530

    Update RA_to_HA

--- right_ascension_to_hour_angle.cpp
+++ right_ascension_to_hour_angle.cpp
@@ -1,3 +1,7 @@
+/**
+ * Book referred
+ * Practical_Astronomy_with_your_Calculator_or_Spreadsheet_4th_edition_by_Peter_Duffett-Smith_Jonathan_Zwart
+ */
 #include <iostream>
 #include <math.h>
 #include <tuple>
@@ -37,6 +41,12 @@ public:
     }
 };
 
+
+/**
+ * Page 9
+ *
+ * To calculate the Julian date corresponding to the Greenwich calendar date
+ */
 ld get_Julian_Day_Number(ld d, ld m, ld y)
 {
     ld _m,_y;
@@ -70,6 +80,10 @@ ld get_Julian_Day_Number(ld d, ld m, ld y)
     return JD;
 }
 
+/**
+ * Page 11
+ * To convert a given Julian date into its counterpart in the Gregorian calendar
+ */
 day_month_year Julian_Date_to_GreenWhich(ld JD)
 {
     JD = JD + 0.5;
@@ -108,36 +122,48 @@ day_month_year Julian_Date_to_GreenWhich(ld JD)
     return day_month_year(day,m,y);
 }
 
+/**
+ * Simply converts given hours,min,sec to decimal hours
+ */
 ld to_decimal_hours(ld hours, ld min, ld sec)
 {
     return hours + (min + (sec/60))/60;
 }
 
+/**
+ * Zone Time is calculated by removing Day Light Saving Time
+ */
 ld to_zone_time(ld hours, ld min, ld sec, ld DST)
 {
     hours = hours - DST;
     return to_decimal_hours(hours, min, sec);
 }
 
+/**
+ * To calculate UT
+ */
 ld get_UT(ld hours, ld time_zone)
 {
     return hours - time_zone;
 }
 
+/**
+ * To get greenwhich calendar day corresponding to the calculated UT
+ */
 ld get_greenwhich_calendar_day(ld local_calendar_day, ld UT)
 {
     return local_calendar_day + (UT/24.0);
 }
 
+/**
+ * Convert the Julian calendar date back into the Greenwich calendar date
+ */
 day_month_year GD_day_month_year(ld greenwhich_calendar_day,ld LCTday, ld LCTmonth, ld LCTyear)
 {
     date d(LCTyear, LCTmonth, LCTday);
     ld JD = get_Julian_Day_Number(greenwhich_calendar_day,LCTmonth,LCTyear);
 
-    std::cout<<std::endl<<"JD: "<<JD<<std::endl;
-
-
-    return Julian_Date_to_GreenWhich(2456474.442);
+    return Julian_Date_to_GreenWhich(JD);
 
 }
 
@@ -148,35 +174,80 @@ ld UT_Final(day_month_year GD)
     return F * 24;
 }
 
-//Conversion of Julian Day to J2000
-ld j2000_format(date a)
+ld get_GST(ld UT, ld GW_day,ld GW_month,ld GW_year)
 {
-    date j2000(2000, 01, 01);
-    return a.julian_day() - j2000.julian_day();
+    GW_day = trunc(GW_day);
+
+    ld JD = get_Julian_Day_Number(GW_day,GW_month,GW_year);
+    ld S = JD - 2451545.0;
+    ld T = S/36525.0;
+    ld _T = 6.697374558 + (2400.051336*T) + (0.000025862*pow(T,2));
+    if(_T > 24)
+        while(_T > 24)  _T = _T - 24;
+    else if(_T < 0)
+        while(_T < 0)   _T = _T + 24;
+
+    UT = UT * 1.002737909;
+
+    ld GST = _T + UT;
+    if(GST > 24)
+        while(GST > 24)  GST = GST - 24;
+    else if(GST < 0)
+        while(GST < 0)   GST = GST + 24;
+
+    return GST;
 }
 
+ld get_LST(ld longitude, ld GST)
+{
+    ld long_hour = longitude/15;
 
-void get_UT_GD(ld LCT_hour,ld LCT_min,ld LCT_sec, ld DST,ld time_zone,
+    long_hour += GST;
+
+    if(long_hour > 24)
+        while(long_hour > 24)  long_hour = long_hour - 24;
+    else if(long_hour < 0)
+        while(long_hour < 0)   long_hour = long_hour + 24;
+    return long_hour;
+}
+
+void get_HA(ld ra, ld longitude,ld LCT_hour,ld LCT_min,ld LCT_sec, ld DST,ld time_zone,
         ld local_calendar_day, ld local_calendar_month, ld local_calendar_year)
 {
     ld zone_time = to_zone_time(LCT_hour,LCT_min,LCT_sec,DST);
-    std::cout<<std::endl<<"ZoneTime: "<<zone_time<<std::endl;
+
     ld temp_UT = get_UT(zone_time,time_zone);
-    std::cout<<std::endl<<"TempUT: "<<temp_UT<<std::endl;
+
     ld GCD = get_greenwhich_calendar_day(local_calendar_day,temp_UT);
-    std::cout<<std::endl<<"GCD: "<<GCD<<std::endl;
+
     day_month_year dmy1 = GD_day_month_year(GCD,local_calendar_day,local_calendar_month,local_calendar_year);
+
     ld ut_final_value = UT_Final(dmy1);
 
-    std::cout<<std::endl<<std::endl<<std::cout<<"GDAY: "<<dmy1.day<<" GMONTH: "<<dmy1.month<<" GYEAR: "<<dmy1.year<<std::endl;
-    std::cout<<"UT: "<<ut_final_value;
+    ld GST = get_GST(temp_UT,trunc(dmy1.day),dmy1.month,dmy1.year);
+
+    ld LST = get_LST(longitude,GST);
+
+    ld Hour_Angle = LST - ra;
+
+    if(Hour_Angle < 0)
+        Hour_Angle = Hour_Angle + 24;
+
+    std::cout<<std::endl<<"HourAngle: "<<Hour_Angle;
+
 }
 
 void take_input()
 {
-    ld LCT_hour, LCT_min, LCT_sec, DST, time_zone,
+    ld ra, longitude, LCT_hour, LCT_min, LCT_sec, DST, time_zone,
             local_calendar_day, local_calendar_month, local_calendar_year;
 
+    std::cout<<"ra: ";
+    std::cin>>ra;
+    std::cout<<std::endl;
+    std::cout<<"Longitude: ";
+    std::cin>>longitude;
+    std::cout<<std::endl;
     std::cout<<"Enter Hour: ";
     std::cin>>LCT_hour>>LCT_min>>LCT_sec;
     std::cout<<std::endl;
@@ -188,7 +259,7 @@ void take_input()
     std::cout<<"Enter DMY: ";
     std::cin>>local_calendar_day>>local_calendar_month>>local_calendar_year;
 
-    get_UT_GD(LCT_hour, LCT_min, LCT_sec, DST, time_zone,
+    get_HA(ra,longitude,LCT_hour, LCT_min, LCT_sec, DST, time_zone,
               local_calendar_day, local_calendar_month, local_calendar_year);
 }
 
@@ -197,4 +268,5 @@ int main()
     freopen("input.txt","r",stdin);
 
     take_input();
+
 }
